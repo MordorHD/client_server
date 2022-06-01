8349b7ac0d519f9b15c97e01f3a1122c48866fc0
MordorHD <53628409+MordorHD@users.noreply.github.com>
2022-06-01 19:49:54 +0400

    Add files via upload

---
+++ client.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <windows.h>
+#include <stdbool.h>
+
+#define IP_ADDRESS "" // your address
+#define PORT 0 // some port
+
+SOCKET user_connect(void)
+{
+    SOCKET sock = INVALID_SOCKET;
+    int iResult;
+
+    struct sockaddr_in addr;
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family       = AF_INET;
+    addr.sin_addr.s_addr  = inet_addr(IP_ADDRESS);
+    addr.sin_port         = htons(PORT);
+
+    sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    iResult = connect(sock, (struct sockaddr*) &addr, sizeof(addr));
+    if(iResult == SOCKET_ERROR)
+    {
+        closesocket(sock);
+        return INVALID_SOCKET;
+    }
+
+    return sock;
+}
+
+int main(int argc, char **argv)
+{
+    SOCKET sock;
+    WSADATA wsaData;
+    INT iRetval;
+    printf("> Connecting to server...\n");
+    if((iRetval = WSAStartup(MAKEWORD(2, 2), &wsaData)))
+    {
+        printf("> error: Connecting failed(error code: 0xF0%x)\n", iRetval);
+        sock = 0;
+    }
+    sock = user_connect();
+    if(sock == INVALID_SOCKET)
+        printf("> error: couldn't connect to server\n");
+    else
+        closesocket(sock);
+    WSACleanup();
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <windows.h>
+#include <stdbool.h>
+
+#define IP_ADDRESS "127.0.01" // your IP address
+#define PORT 0 // your port
+
+SOCKET start_server(void)
+{
+    INT iResult;
+    SOCKET sock;
+
+    struct sockaddr_in addr;
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock == INVALID_SOCKET)
+    {
+        printf("> error: socket failed (error code: %d)\n", WSAGetLastError());
+        return INVALID_SOCKET;
+    }
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family       = AF_INET;
+    addr.sin_addr.s_addr  = inet_addr(IP_ADDRESS);
+    addr.sin_port         = htons(PORT);
+
+    iResult = bind(sock, (struct sockaddr*) &addr, sizeof(addr));
+    if(iResult == SOCKET_ERROR)
+    {
+        printf("> error: bind failed (error code: %d)\n", WSAGetLastError());
+        closesocket(sock);
+        return INVALID_SOCKET;
+    }
+    return sock;
+}
+
+int main(int argc, char **argv)
+{
+    char buf[1024];
+    SOCKET serverSock;
+    SOCKET socks[2];
+    WSADATA wsaData;
+    INT iRetval;
+    if((iRetval = WSAStartup(MAKEWORD(2, 2), &wsaData)))
+    {
+        printf("> error: Startup failed(error code: 0xF0%x)\n", iRetval);
+        return 0xFF;
+    }
+    printf("> Connecting to server...\n");
+    serverSock = start_server();
+    if(serverSock == INVALID_SOCKET)
+    {
+        printf("> error: invalid server socket\n");
+        WSACleanup();
+        return 0xFF;
+    }
+    printf("> Connected to server...\n");
+    printf("> Listening for connections..\n");
+    // wait for two clients two join
+    for(int i = 0; i < sizeof(socks) / sizeof(*socks); )
+    {
+        iRetval = listen(serverSock, 0);
+        if(iRetval == SOCKET_ERROR)
+        {
+            printf("> error: listen\n");
+            continue;
+        }
+        socks[i] = accept(serverSock, NULL, NULL);
+        if(socks[i] == INVALID_SOCKET)
+            continue;
+        printf("> Client %d connected\n", i);
+        i++;
+    }
+    return 0;
+}
